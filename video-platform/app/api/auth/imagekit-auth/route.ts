b89f02a41c0aedf2f331a434d810570359b547fc
Rupayan2005 <rupayanauddya2023@gmail.com>
2025-06-30 13:11:19 +0530

    imagekit auth file added

--- video-platform/app/api/auth/imagekit-auth/route.ts
+++ video-platform/app/api/auth/imagekit-auth/route.ts
@@ -2,15 +2,12 @@ import { getUploadAuthParams } from "@imagekit/next/server";
 
 export async function GET() {
   try {
-    const authenticationParameters = getUploadAuthParams({
+    const { signature, expire, token } = getUploadAuthParams({
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY as string,
     });
-  
-    return Response.json({
-      authenticationParameters,
-      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-    });
+
+    return Response.json({ signature, expire, token });
   } catch (error) {
     console.error("Error generating ImageKit authentication parameters:", error);
     return Response.json(
